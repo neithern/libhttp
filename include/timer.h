1896cdc48f04b61afd66f2e1469f3c31cba7b27d
Nanling <zhengnanling@bilibili.com>
2020-07-20 16:11:52 +0800

    support timer

---
+++ include/timer.h
@@ -0,0 +1,32 @@
+#ifndef _file_reader_h_
+#define _file_reader_h_
+
+#include <functional>
+
+typedef struct uv_loop_s uv_loop_t;
+typedef struct uv_timer_s uv_timer_t;
+
+namespace http
+{
+
+class timer
+{
+public:
+    timer(std::function<void()> callback, uv_loop_t* loop = nullptr);
+    ~timer();
+
+    bool start(uint64_t timeout, uint64_t repeat = 0);
+    bool stop();
+
+private:
+    static void timer_cb(uv_timer_t* handle);
+
+private:
+    std::function<void()> callback_;
+    uv_timer_t* timer_;
+    bool started_;
+};
+
+}
+
+#endif // _file_map_h_
---
+++ src/timer.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <uv.h>
+#include "timer.h"
+
+namespace http
+{
+
+timer::timer(std::function<void()> callback, uv_loop_t* loop)
+{
+    callback_ = callback;
+    started_ = false;
+    timer_ = new uv_timer_s{};
+    uv_handle_set_data((uv_handle_t*)timer_, this);
+
+    if (loop == nullptr)
+        loop = uv_default_loop();
+    uv_timer_init(loop, timer_);
+}
+
+timer::~timer()
+{
+    if (started_)
+        uv_timer_stop(timer_);
+    delete timer_;
+}
+
+bool timer::start(uint64_t timeout, uint64_t repeat)
+{
+    if (!started_)
+        started_ = uv_timer_start(timer_, timer_cb, timeout, repeat) == 0;
+    return started_;
+}
+
+bool timer::stop()
+{
+    if (started_)
+        started_ = uv_timer_stop(timer_) == 0;
+    return !started_;
+}
+
+void timer::timer_cb(uv_timer_t* handle)
+{
+    timer* p_this = (timer*)uv_handle_get_data((uv_handle_t*)handle);
+    p_this->callback_();
+}
+
+} // namespace http
