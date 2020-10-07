c4ea660269b7f8e45bcc47bba1f124e3ac37a599
Nanling <neithern@outlook.com>
2020-10-08 01:12:53 +0800

    return int for worker

--- src/worker.cpp
+++ src/worker.cpp
@@ -8,8 +8,9 @@ namespace http
 
 struct work_req : public uv_work_t
 {
-    std::function<void()> work;
-    std::function<void(int)> after_work;
+    std::function<int()> work;
+    std::function<void(int)> done;
+    int result;
 };
 
 worker::worker(uv_loop_t* loop)
@@ -17,11 +18,11 @@ worker::worker(uv_loop_t* loop)
     loop_ = loop != nullptr ? loop : uv_default_loop();
 }
 
-bool worker::queue(std::function<void()>&& work, std::function<void(int)>&& after_work)
+bool worker::queue(std::function<int()>&& work, std::function<void(int)>&& done)
 {
     work_req* req = new work_req{};
     req->work = std::move(work);
-    req->after_work = std::move(after_work);
+    req->done = std::move(done);
     uv_handle_set_data((uv_handle_t*)req, req);
     return uv_queue_work(loop_, req, worker_cb, after_worker_cb) == 0;
 }
@@ -30,16 +31,16 @@ void worker::worker_cb(uv_work_t* req)
 {
     work_req* p_req = (work_req*)req;
     if (p_req->work)
-        p_req->work();
+        p_req->result = p_req->work();
     p_req->work = nullptr;
 }
 
 void worker::after_worker_cb(uv_work_t* req, int status)
 {
     work_req* p_req = (work_req*)req;
-    if (p_req->after_work)
-        p_req->after_work(status);
-    p_req->after_work = nullptr;
+    if (p_req->done)
+        p_req->done(p_req->result);
+    p_req->done = nullptr;
     delete p_req;
 }
 
